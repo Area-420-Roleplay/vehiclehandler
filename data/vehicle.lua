003a48a5051f65fb840e1c22f89352a18e198fd4
XxKRONICxX <160303106+XxKRONICxX@users.noreply.github.com>
2025-01-09 03:34:25 -0600

    .

--- data/vehicle.lua
+++ data/vehicle.lua
@@ -8,29 +8,29 @@ return {
     },
     globalmultiplier = 20.0,    -- Base damage multiplier for all vehicles (lower value = less damage)
     classmultiplier = {         -- Add-on damage multiplier for vehicle classes
-        [0] =   1.0,            -- 0: Compacts
-                1.0,            -- 1: Sedans
-                1.0,	        -- 2: SUVs
-                0.95,	        -- 3: Coupes
-                1.0,	        -- 4: Muscle
-                0.95,           -- 5: Sports Classics
-                0.95,	        -- 6: Sports
-                0.95,	        -- 7: Super
-                0.47,	        -- 8: Motorcycles
-                0.7,	        -- 9: Off-road
-                0.25,	        -- 10: Industrial
-                0.35,	        -- 11: Utility
-                0.85,	        -- 12: Vans
-                1.0,	        -- 13: Bicycles
-                0.4,	        -- 14: Boats
-                0.7,	        -- 15: Helicopters
-                0.7,	        -- 16: Planes
-                0.75,	        -- 17: Service
-                0.35,	        -- 18: Emergency
-                0.27,	        -- 19: Military
-                0.43,	        -- 20: Commercial
-                0.1,	        -- 21: Trains
-                1.0,	        -- 22: Open Wheel
+        [0] =   0.5,            -- 0: Compacts
+                0.5,            -- 1: Sedans
+                0.5,	        -- 2: SUVs
+                0.45,	        -- 3: Coupes
+                0.5,	        -- 4: Muscle
+                0.45,           -- 5: Sports Classics
+                0.45,	        -- 6: Sports
+                0.45,	        -- 7: Super
+                0.22,	        -- 8: Motorcycles
+                0.35,	        -- 9: Off-road
+                0.12,	        -- 10: Industrial
+                0.16,	        -- 11: Utility
+                0.42,	        -- 12: Vans
+                0.5,	        -- 13: Bicycles
+                0.2,	        -- 14: Boats
+                0.35,	        -- 15: Helicopters
+                0.35,	        -- 16: Planes
+                0.38,	        -- 17: Service
+                0.17,	        -- 18: Emergency
+                0.13,	        -- 19: Military
+                0.21,	        -- 20: Commercial
+                0.05,	        -- 21: Trains
+                0.5,	        -- 22: Open Wheel
     },
     regulated = {               -- Prevent controls for vehicle class while flipped/airborne
         [0] =   true,           -- 0: Compacts
